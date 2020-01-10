e89689b08e207b983ad0627eb8b0ebebc4854033
Alexander Paschout <alexander.paschout@voestalpine.com>
2020-01-10 12:37:45 +0100

    add DeckID type; add Deck handlers

--- deck.go
+++ deck.go
@@ -1,17 +1,22 @@
 package tournaments
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/cognicraft/hyper"
+	"github.com/cognicraft/uuid"
 )
 
 type Deck struct {
-	Id   string
+	ID   DeckID
 	Name string
 	Link string
 }
+
+type DeckID string
 
 func (s *Server) handleGETDecks(w http.ResponseWriter, r *http.Request) {
 	var err error
@@ -33,34 +38,116 @@ func (s *Server) handleGETDecks(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	for _, dck := range s.decks {
-		item := hyper.Item{
-			Label: dck.Name,
-			Type:  "deck",
-			Properties: []hyper.Property{
-				{
-					Label: "ID",
-					Name:  "id",
-					Value: dck.Id,
-				},
-				{
-					Label: "Name",
-					Name:  "name",
-					Value: dck.Name,
-				},
-				{
-					Label: "Deck-Builder",
-					Name:  "link",
-					Value: dck.Link,
-				},
-			},
-		}
-		dLink := hyper.Link{
-			Rel:  "details",
-			Href: resolve("./%d", dck.Id).String(),
-		}
-		item.AddLink(dLink)
+		item := dck.MakeUndetailedHyperItem(resolve)
 		res.AddItem(item)
 	}
 	res.AddLinks(links)
 	hyper.Write(w, http.StatusOK, res)
+}
+
+func (s *Server) handleGetDeck(w http.ResponseWriter, r *http.Request) {
+	resolve := hyper.ExternalURLResolver(r)
+	dID := DeckID(r.Context().Value(":id").(string))
+
+	dck, err := s.db.FindDeckByID(dID)
+	if err != nil {
+		log.Println(err)
+		hyper.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	res := dck.MakeDetailedHyperItem(resolve)
+	hyper.Write(w, http.StatusOK, res)
+}
+
+func (s *Server) handlePOSTDecks(w http.ResponseWriter, r *http.Request) {
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		hyper.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	dck := Deck{}
+	err = json.Unmarshal(b, &dck)
+	if err != nil {
+		log.Println(err)
+		hyper.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	ok, err := s.db.DeckNameAvailable(dck.Name)
+	if !ok {
+		log.Println(err)
+		hyper.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	_, err = uuid.Parse(string(dck.ID))
+	if err != nil {
+		dck.ID = DeckID(uuid.MakeV4())
+	}
+	dck.ID = DeckID(uuid.MakeV4())
+	err = s.db.SaveDeck(dck)
+	if err != nil {
+		log.Println(err)
+		hyper.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (dck *Deck) MakeUndetailedHyperItem(resolve hyper.ResolverFunc) hyper.Item {
+	item := hyper.Item{
+		Label: dck.Name,
+		Type:  "deck",
+		Properties: []hyper.Property{
+			{
+				Label: "ID",
+				Name:  "id",
+				Value: dck.ID,
+			},
+			{
+				Label: "Name",
+				Name:  "name",
+				Value: dck.Name,
+			},
+		},
+	}
+	dLink := hyper.Link{
+		Rel:  "details",
+		Href: resolve("./%s", dck.ID).String(),
+	}
+	item.AddLink(dLink)
+	return item
+}
+
+func (dck *Deck) MakeDetailedHyperItem(resolve hyper.ResolverFunc) hyper.Item {
+	item := hyper.Item{
+		Label: dck.Name,
+		Type:  "deck",
+		Properties: []hyper.Property{
+			{
+				Label: "Name",
+				Name:  "name",
+				Value: dck.Name,
+			},
+			{
+				Label: "ID",
+				Name:  "id",
+				Value: dck.ID,
+			},
+			{
+				Label: "Deck-Builder",
+				Name:  "link",
+				Value: dck.Link,
+			},
+		},
+	}
+	link := hyper.Link{
+		Rel:  "self",
+		Href: resolve("./%s", dck.ID).String(),
+	}
+	item.AddLink(link)
+	return item
 }

713b925758650f3a667170fd9c7d6efab8a6e412
Alexander Paschout <alexander.paschout@voestalpine.com>
2020-01-28 13:39:46 +0100

    Move subscription to server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,28 +10,24 @@ import (
 )
 
 const (
-	dsn           = ":memory:"
-	dsnEventStore = "events.db"
-	port          = ":8080"
+	storeDSN  = ":memory:"
+	eventsDSN = "events.db"
+	port      = ":8080"
 )
 
 func main() {
-	es, err := event.NewStore(dsnEventStore)
+	es, err := event.NewStore(eventsDSN)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer es.Close()
-	sub := es.SubscribeToStreamFromCurrent(event.All)
-	defer sub.Cancel()
 
-	db, err := tournaments.NewDB(dsn)
+	db, err := tournaments.NewStore(storeDSN)
 	if err != nil {
 		log.Fatal(err)
 	}
-	sub.On(db.On)
 
 	s := tournaments.NewServer(db, es)
-	s.Init()
 	fmt.Printf("Server running on %s\n", port)
 	err = http.ListenAndServe(port, s)
 	if err != nil {
